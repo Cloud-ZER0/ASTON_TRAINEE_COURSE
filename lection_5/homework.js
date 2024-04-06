29b5dd6c5092de53768a15232e60bbeac07037f4
29453komandor@gmail.com <29453komandor@gmail.com>
2024-04-06 23:14:48 +0300

    add solution to homework 5

--- lection_5/homework.js
+++ lection_5/homework.js
@@ -1,33 +1,32 @@
 //1.
-// const a = {b: 1},
-//     c = Object.create(a);
-//
-// console.log(c.b); // ?
-// delete c.b;
-// console.log(c.b); // ?
-// delete a.b;
-// console.log(c.b); // ?
-// a.z = 2;
-// console.log(c.z); // ?
-// c.z = 3;
-// console.log(a.z); // ?
+const a = { b: 1 },
+  c = Object.create(a);
+
+console.log(c.b); // ? 1
+delete c.b;
+console.log(c.b); // ? 1 // Тк метод находится в объекте родителя
+delete a.b;
+console.log(c.b); // ? undedined
+a.z = 2;
+console.log(c.z); // ? 2
+c.z = 3;
+console.log(a.z); // ? 2 тк, переопределение поля у дочернего объекта не изменяет поле родительского
 
 // 2.
 
-// const promise = new Promise(() => {
-// })
-// promise.prototype === Promise.__proto__ // ?
-//
-// const obj = {}
-// obj.__proto__ === Object.prototype // ?
-//
-// new Array([]).__proto__ === Array.prototype // ?
-//
-// function Fn1 () {}
-// function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
-//
-// Fn1.prototype === Fn2.prototype // ?
+const promise = new Promise(() => {});
+promise.prototype === Promise.__proto__; // false
+
+const obj = {};
+obj.__proto__ === Object.prototype; // true
+
+new Array([]).__proto__ === Array.prototype; // true
+
+function Fn1() {}
+function Fn2() {}
+console.log(Fn1.constructor === Fn2.constructor); // true
+
+console.log(Fn1.prototype === Fn2.prototype); // false
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
@@ -49,3 +48,59 @@
 // bird.speak();   // "Some generic sound"
 // bird.fly();     // "Flying high!"
 
+//SOLUTION 1
+
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     speak() {
+//         console.log('Some generic sound');
+//     }
+// }
+
+// class Bird extends Animal {
+//     constructor(name) {
+//         super.name = name;
+//     }
+//     fly() {
+//         console.log('Flying high!');
+//     }
+// }
+
+// const animal = new Animal("Дженни");
+// const bird = new Bird("Воробей");
+
+// animal.speak(); // "Some generic sound"
+// bird.speak();   // "Some generic sound"
+// bird.fly();     // "Flying high!"
+
+// SOLUTION 2
+
+function Animal(name) {
+  this.name = name;
+}
+
+// Тут даже среда разработки подсказывает, что This constructor function may be converted to a class declaration
+// насколько вообще имеет смысл усложнять себе жизнь используя функции и передавая this напрямую?
+
+Animal.prototype.speak = () => {
+  console.log("Some generic sound");
+};
+
+function Bird(name) {
+  Animal.call(this, name);
+}
+
+Bird.prototype.fly = function () {
+  console.log("Flying high!");
+};
+
+Object.setPrototypeOf(Bird.prototype, Animal.prototype);
+
+const animal = new Animal("Дженни");
+const bird = new Bird("Воробей");
+
+animal.speak(); // "Some generic sound"
+bird.speak(); // "Some generic sound"
+bird.fly(); // "Flying high!"
